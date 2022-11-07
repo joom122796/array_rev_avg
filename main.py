2d2857e4e643e72289c96aa8a55b7eb1d8b42423
joom <113173627+joom122796@users.noreply.github.com>
2022-11-07 16:05:29 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+#Write a program to read 6 numbers into an array numbers[0] to numbers[5], ouput them in reverse order and then output the total and average.
+import random
+
+arr = [0]
+
+for i in range(0,6):
+  num = random.randint(0,500)
+  arr.append(num)
+  i = i + 1
+else:
+  arr.pop(0)
+  print(f'Your array is {arr}')
+  arr.reverse()
+  print(f'Your reversed array is: {arr}')
+  arravg = sum(arr)/i
+  print(f'The average of the array is: {arravg}')
